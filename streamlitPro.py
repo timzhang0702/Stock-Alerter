be820c0fff5b6970b5f1b005d94639344358a149
timzhang0702 <80072838+timzhang0702@users.noreply.github.com>
2021-06-13 14:38:08 -0400

    Update streamlitPro.py

--- streamlitPro.py
+++ streamlitPro.py
@@ -219,7 +219,7 @@ def table(rows, companyName, sector, volume, rows2, companyName2, sector2, volum
     fig.update_layout(height=(len(companyName2)) * 21 + 25, margin=dict(l=1, r=1, b=0, t=0))
     st.write(fig)
 
-
+@st.cache()
 def twitter(stock):
     endpoint = 'https://api.twitter.com/2/tweets/search/recent'
     headers = {'authorization': f'Bearer {BEARER_TOKEN}'}
@@ -245,7 +245,7 @@ def twitter(stock):
         return data
 
     now = datetime.now()  # get the current datetime, this is our starting point
-    last_week = now - timedelta(days=5)  # datetime one week ago = the finish line
+    last_week = now - timedelta(days=1)  # datetime one week ago = the finish line
     now = now.strftime(dtformat)  # convert now datetime to format for API
     df = pd.DataFrame()  # initialize dataframe to store tweets
     while True:
@@ -283,7 +283,7 @@ def twitter(stock):
     # mean_scores = mean_scores.unstack()
     return mean_scores
 
-
+@st.cache()
 def ta(stock):
     layout = dict(
         xaxis=dict(rangeslider=dict(visible=False), categoryorder="category ascending", type="category",
@@ -385,9 +385,6 @@ try:
             table(rows, companyName, sector, volume, rows2, companyName2, sector2, volume2)
             footer()
         else:
-            taWrite()
-            fig, config = ta(tickerSymbol)
-            st.plotly_chart(fig, config=config)
             range = st.sidebar.selectbox("Date Range", (
                 '1 Day', '5 Days', '1 Month', '3 Months', '6 Months', 'YTD', '1 Year', '5 Years', 'Max'), 3)
             string_name, string_summary, fig, config = chart(tickerData, range)
