1c9e448dc16c11e81f34c634f2879cbb83496e18
timzhang0702 <80072838+timzhang0702@users.noreply.github.com>
2021-08-07 14:24:19 -0400

    Update streamlitPro.py

--- streamlitPro.py
+++ streamlitPro.py
@@ -582,7 +582,34 @@ if option == 'Search':
         st.info('Page In Development')
 
 
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
 
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Heflin Stephen Raj S</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
 
 
 
