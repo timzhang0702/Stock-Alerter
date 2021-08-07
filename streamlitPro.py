4c26e052828854979406f847f802be078b21bec4
timzhang0702 <80072838+timzhang0702@users.noreply.github.com>
2021-08-07 14:35:28 -0400

    Update streamlitPro.py

--- streamlitPro.py
+++ streamlitPro.py
@@ -210,7 +210,7 @@ def table(Symbol, Open, High, Low, Close, Volume, pctchange):
                                  fill_color='#E5ECF6', align='left')))
     fig.update_layout(height=(len(Open)) * 21 , margin=dict(l=1, r=1, b=0, t=0))
     st.write(fig)
-    st.write("---")
+    
 
 @st.cache()
 def twitter(stock):
